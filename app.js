f14d02a2dae4fb8060f507577766a96e0b4ee99a
Casey Carroll <caseydean8@gmail.com>
2020-04-27 11:28:44 -0400

    Added algebra argument for basic equations

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
-// * Create a command-line node application that takes in parameters like this:
 
-//   * `node calculator.js add 1 2` ... and outputs 3
-//   * `node calculator.js subtract 5 2` ... and outputs 3
-//   * `node calculator.js multiply 3 2` ... and outputs 6
-//   * `node calculator.js divide 8 2` ... and outputs 4
-//   * `node calculator.js remainder 7 2`... and outputs 1
-
-// ### Bonuses
-
-// * Enable your calculator application to also handle the below cases:
-
-//   * `node calculator.js exp 7 2` ... and output 49 (7 squared)
-//   * `node calculator.js algebra 4x+2=10`... and output 2. (Hint: Assume the algebra will always be in this exact form and will always be addition)
 const pa = process.argv;
 
-let operator = pa[2];
+const operator = pa[2];
+const algArg = pa[3];
 let firstNum = parseInt(pa[3]);
 let secondNum = parseInt(pa[4]);
+
+const parseAlgebra = equation => {
+  const xIndex = equation.indexOf("x");
+  const equalsIndex = equation.indexOf("=");
+  firstNum = parseInt(equation.substring(0, xIndex));
+  secondNum = parseInt(equation.substring(xIndex + 2, equalsIndex));
+  const thirdNum = parseInt(
+    equation.substring(equalsIndex + 1, equation.length)
+  );
+  let result;
+  for (let element of equation) {
+    switch (element) {
+      case "+":
+        result = (thirdNum - secondNum) / firstNum;
+        break;
+      case "-":
+        result = (thirdNum + secondNum) / firstNum;
+        break;
+      case "*":
+        result = thirdNum / secondNum / firstNum;
+        break;
+      case "/":
+        result = (thirdNum * secondNum) / firstNum;
+        break;
+    }
+  }
+  return result;
+};
 
 switch (operator) {
   case "add":
@@ -34,4 +50,12 @@ switch (operator) {
   case "remainder":
     console.log(firstNum % secondNum);
     break;
+  case "exp":
+    console.log(firstNum ** secondNum);
+    break;
+  case "algebra":
+    console.log(parseAlgebra(algArg));
+    break;
+  default:
+    console.log("input isn't understood");
 }
